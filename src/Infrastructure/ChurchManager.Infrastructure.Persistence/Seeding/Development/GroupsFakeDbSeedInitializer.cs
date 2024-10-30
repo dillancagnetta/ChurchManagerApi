e58a77144a093b7897059bd8a302202d38fcdd42
dillancagnetta <dillancagnetta@yahoo.com>
2024-10-30 10:28:55 +0200

    fix group icons

--- src/Infrastructure/ChurchManager.Infrastructure.Persistence/Seeding/Development/GroupsFakeDbSeedInitializer.cs
+++ src/Infrastructure/ChurchManager.Infrastructure.Persistence/Seeding/Development/GroupsFakeDbSeedInitializer.cs
@@ -20,8 +20,8 @@ namespace ChurchManager.Infrastructure.Persistence.Seeding.Development
         int personId = 2;
 
         // Cell Group Type
-        private readonly GroupType _sectionGroupType = new() { Name = "Section", Description = "Group Section", IconCssClass = "heroicons_outline:collection" };
-        private readonly GroupType _cellGroupType = new() { Name = "Cell", Description = "Cell Ministry", IconCssClass = "heroicons_outline:share" };
+        private readonly GroupType _sectionGroupType = new() { Name = "Section", Description = "Group Section", IconCssClass = "heroicons_outline:volume-up" };
+        private readonly GroupType _cellGroupType = new() { Name = "Cell", Description = "Cell Ministry", IconCssClass = "heroicons_outline:volume-off" };
 
         private GroupTypeRole _cellLeaderRole;
         private GroupTypeRole _cellAssistantRole;
