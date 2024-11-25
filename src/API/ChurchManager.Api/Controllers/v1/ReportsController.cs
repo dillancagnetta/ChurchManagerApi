f63762e9c69e61f05c4ddfec1ee65f2a947c9e18
dillancagnetta <dillancagnetta@yahoo.com>
2024-11-25 11:13:55 +0200

    Added test report

---
+++ src/API/ChurchManager.Api/Controllers/v1/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChurchManager.Api.Controllers.v1;
+
+[ApiVersion("1.0")]
+[AllowAnonymous]
+public class ReportsController: BaseApiController
+{
+    [HttpGet()]
+    public async Task<IActionResult> GetSampleReport( CancellationToken token)
+    {
+        /*LocalReport report = new LocalReport();
+        report.LoadReportDefinition(reportDefinition);
+        report.DataSources.Add(new ReportDataSource("source", dataSource));
+        report.SetParameters(new[] { new ReportParameter("Parameter1", "Parameter value") });
+        byte[] pdf = report.Render("PDF");*/
+    }
+}
